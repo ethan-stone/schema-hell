ee7edfe5defed0daaf1c48fdebdfdfac0ea18f79
ethan-stone <ethan.stone9352@gmail.com>
2022-11-16 00:22:36 -0500

    fix: staging user arn

--- stacks/env.ts
+++ stacks/env.ts
@@ -1,7 +1,7 @@
 export const env = {
   staging: {
     AWS_IAM_WEB_BACKEND_USER_ARN:
-      "arn:aws:iam::475216627762:user/schema-hell-web-backend-user-dev"
+      "arn:aws:iam::475216627762:user/schema-hell-web-backend-user-staging"
   },
   prod: {
     AWS_IAM_WEB_BACKEND_USER_ARN:
