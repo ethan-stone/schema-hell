3ad68d8e102724af2e913e804f158b528213e1cf
Ethan Stone <46607985+ethan-stone@users.noreply.github.com>
2022-11-18 10:14:40 -0500

    add back global css for now

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
+import "../styles/globals.css";
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
