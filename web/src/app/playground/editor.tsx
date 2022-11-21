45782bbb32d90c6cde5d0351d9048e2844188d0e
ethan-stone <ethan.stone9352@gmail.com>
2022-11-20 23:06:00 -0500

    remove orange and indigo colors

--- web/src/app/playground/editor.tsx
+++ web/src/app/playground/editor.tsx
@@ -24,7 +24,7 @@ const Select = <T,>(props: SelectProps<T>) => {
       }}
     >
       <div className="relative mt-1">
-        <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+        <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-neutral-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-neutral-700 sm:text-sm">
           <span className="block truncate">
             {mapOptionDisplayName(selected).displayName}
           </span>
