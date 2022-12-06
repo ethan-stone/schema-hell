72e1140d23b9bcd591b2c2e1e737fcd748b37f20
ethan-stone <ethan.stone9352@gmail.com>
2022-12-05 21:49:00 -0500

    added more to things to know

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-10 bg-gradient-to-b from-neutral-900 via-neutral-800 to-neutral-900 font-mono text-white">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-10 bg-gradient-to-b bg-neutral-900 font-mono text-white">
       <div className="text-9xl">SCHEMA HELL</div>
       <div className="text-3xl">
         Schema evolution and compatibility is hard. This helps to make it easier
