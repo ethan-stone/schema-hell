96a7de04587b9e1f7692ff10de4ec3018a9f6fd1
ethan-stone <ethan.stone9352@gmail.com>
2022-12-05 22:47:08 -0500

    updated responsiveness

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-10 bg-neutral-900 bg-gradient-to-b font-mono text-white">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-10 bg-neutral-900 bg-gradient-to-b text-center font-mono text-white">
       <div className="text-9xl">SCHEMA HELL</div>
-      <div className="flex w-1/3 flex-row gap-6">
+      <div className="flex flex-col items-center gap-6 md:w-3/4 lg:w-1/3 lg:flex-row lg:items-stretch">
         <div className="flex w-1/2 flex-grow flex-col items-center justify-between gap-10 rounded border border-neutral-200 p-10 text-center">
           <p>Easily test JSON Schema compatibility</p>
           <Link href="/compatibility-checker">/compatibility-checker</Link>
