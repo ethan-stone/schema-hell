9c498bdbd4ddb12d73afa7c94d81a89b34d4eaed
ethan-stone <ethan.stone9352@gmail.com>
2022-12-05 22:22:08 -0500

    added new head to html document

---
+++ web/src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Schema Hell</title>
+      <meta>
+        Test schema compatibility and learn from my mistakes on developing
+      </meta>
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
